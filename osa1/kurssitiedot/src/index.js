f0d8b58316f6897cb42931e8d3d340c15e2a3910
jthaapas <jari.t.haapasaari@student.jyu.fi>
2020-04-28 21:30:14 +0300

    Tehtävä 1.1

---
+++ osa1/kurssitiedot/src/index.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Part = (props) => {
+
+  return (
+    <div>
+        {props.part1} {props.exercises1}
+        {props.part2} {props.exercises2}
+        {props.part3} {props.exercises3}
+    </div>
+  )
+}
+
+const Header = (props) => {
+
+  return (
+    <div>
+        <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+
+  return (
+    <div>
+      <p>
+        {props.part1} {props.exercises1}
+        <br />
+        <br />
+        {props.part2} {props.exercises2}
+        <br />
+        <br />
+        {props.part3} {props.exercises3}
+        <br />
+        <Part  />
+        <Part  />
+        <Part {props.part1} {props.exercises1} />
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+
+  return (
+    <div>
+      <p>
+      {props.exercisesTitle} {props.totalExercises}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const exercisesTitle = 'Number of exercises'
+  const totalExercises = exercises1 + exercises2 + exercises3
+
+  return (
+    <div>
+      <Header course = { course } />
+      <Content part1 = {part1} exercises1 = {exercises1}
+               part2 = {part2} exercises2 = {exercises2}
+               part3 = {part3} exercises3 = {exercises3} />
+      <Total exercisesTitle = {exercisesTitle}
+             totalExercises = {totalExercises} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
