ad019370bc0828cc4d6447933e352e4d54a64138
Haspe <haspe@haspe.dev>
2020-07-03 22:17:23 +0000

    Eteenpäin

---
+++ prac/osa1/js/oliot.js
@@ -0,0 +1,23 @@
+const object = {
+    name: 'Test',
+    age: 35,
+    education: 'Tohtori',
+}
+
+const object2 = {
+    name: 'Test',
+    age: 35,
+    education: 'Tohtori',
+}
+
+const object3 = {
+    name: {
+        first: 'Juha',
+        last: 'Tauriainen',
+    },
+    grades: [1, 2, 3, 4],
+    department: 'TKTL',
+}
+
+console.log(object.name)
+
--- prac/osa1/kertaus/src/index.js
+++ prac/osa1/kertaus/src/index.js
@@ -2,21 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
-  )
-}
+
+    const bornYear = () => {
+      
+      const yearNow = new Date().getFullYear()
+      return yearNow - props.age
+    }
+
+    return (
+      <div>
+        <p>
+          Hello {props.name}, you are {props.age} years old
+        </p>
+        <p>So you were probably born {bornYear()}</p>
+      </div>
+    )
+  }
 
 const App = () => {
 
   const ika = 20
   const nimi = "Aku"
 
-  // console.log('Hello from komponentti')
+  console.log('Hello from komponentti')
   return (
     <div>
+      {console.log("Hello")}
       <h1>Greetings</h1>
       <Hello name="Iines" age={ika}/>
       <Hello name={nimi} age={ika}/>
