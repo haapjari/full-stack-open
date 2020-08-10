740c927ae57ce212fa1c67ab289d4d855cc93be4
jthaapas <jthaapas@student.jyu.fi>
2020-08-10 20:17:41 +0000

    Teht. 1.6 KESKEN

--- osa1/unicafe-palautesovellus/src/index.js
+++ osa1/unicafe-palautesovellus/src/index.js
@@ -1,15 +1,39 @@
-import React, { useState } from 'react';
+import React , { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-// Components here
+// Components START
 
-const App = () => {
+const Button = () => {
 
-  // Variables here
+}
+
+// Components END
+
+const App = (props) => {
+
+  // Variables START
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setToGood = (newGood) => {
+    setGood(newGood)
+  }
+
+  // Variables END
 
   return (
     <div>
-      <p>Hello World</p>
+      <h1>give feedback</h1>
+      <button onClick={() => setToGood(good + 1)}>good</button>
+      <button>neutral</button>
+      <button>bad</button>
+
+      <h1>statistics</h1>
+      <p>good</p>{good}
+      <p>neutral</p>{neutral}
+      <p>bad</p>{bad}
     </div>
   )
 }
