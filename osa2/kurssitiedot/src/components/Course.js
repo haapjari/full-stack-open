e9d64a7587b3b935b732c3c959cfe7138f445acd
Haspe <haapjari@gmail.com>
2020-09-28 02:16:06 +0300

    Kurssitiedot 2.2 Step 7

--- osa2/kurssitiedot/src/components/Course.js
+++ osa2/kurssitiedot/src/components/Course.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import Content from './Content'
+import Total from './Total'
 
 const Course = (props) => {
 
@@ -8,6 +9,7 @@ const Course = (props) => {
       <div>
         <Header course={props.course} />
         <Content course={props.course} />
+        <Total course={props.course} />
       </div>
     )
   }
--- osa2/kurssitiedot/src/components/Total.js
+++ osa2/kurssitiedot/src/components/Total.js
@@ -2,13 +2,16 @@ import React from 'react'
 
 const Total = (props) => {
 
+    var length = props.course.parts.length;
+    var totalExercises = 0;
+
+    for (var i = 0; i < length; i++) {
+        totalExercises += props.course.parts[i].exercises;
+    }
+
     return (
       <div>
-        <p>
-        {"Number of exercises"} {props.course.parts[0].exercises +
-                                 props.course.parts[1].exercises + 
-                                 props.course.parts[2].exercises}
-        </p>
+        <b>Number of exercises: {totalExercises} </b>
       </div>
     )
   }
