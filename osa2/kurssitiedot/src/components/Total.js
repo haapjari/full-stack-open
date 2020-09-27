c45dfa2083dfb1f71b8702f5893e65b80862c674
Haspe <haapjari@gmail.com>
2020-09-28 02:32:45 +0300

    Kurssitiedot 2.4 Step 8

--- osa2/kurssitiedot/src/components/Total.js
+++ osa2/kurssitiedot/src/components/Total.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
+function myAddition(total, num) {
+    return total + num;
+}
+
 const Total = (props) => {
 
     var length = props.course.parts.length;
     var totalExercises = 0;
+    var numbers = []
 
     for (var i = 0; i < length; i++) {
-        totalExercises += props.course.parts[i].exercises;
+        numbers.push(props.course.parts[i].exercises);
     }
+
+    totalExercises = numbers.reduce(myAddition);
 
     return (
       <div>
