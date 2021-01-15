0a388cf071b1c45f24fe8b855fce638515514c8e
jthaapas <jthaapas@student.jyu.fi>
2021-01-15 16:13:24 +0200

    Full React Course 2020 - 1:07 - 2:19

--- freeCodeCamp/Course/tutorial/src/index.js
+++ freeCodeCamp/Course/tutorial/src/index.js
@@ -1,8 +1,37 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-function Greeting() {
-  return <h4>Test</h4>;
+// CSS
+
+import './style.css'
+
+// Variables
+
+// const title = "Harry Potter";
+// const author = "J.K. Rowling";
+// const img = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fstatic3.businessinsider.com%2Fimage%2F511cffe5eab8ea1355000012-948-1445%2Fnew%2520harry%2520potter%2520cover.jpg&f=1&nofb=1";
+
+// always return JSX
+
+function BookList() {
+  return (
+    <section className='booklist'>
+      <Book title="Game of Thrones" author="Test Author" img="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fstatic3.businessinsider.com%2Fimage%2F511cffe5eab8ea1355000012-948-1445%2Fnew%2520harry%2520potter%2520cover.jpg&f=1&nofb=1"/>
+      <Book title="Star Wars" author="Second Author" img="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fstatic3.businessinsider.com%2Fimage%2F511cffe5eab8ea1355000012-948-1445%2Fnew%2520harry%2520potter%2520cover.jpg&f=1&nofb=1"/>
+      <Book title="Narnia" author="Third Author" img="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fstatic3.businessinsider.com%2Fimage%2F511cffe5eab8ea1355000012-948-1445%2Fnew%2520harry%2520potter%2520cover.jpg&f=1&nofb=1"/>
+    </section>
+  )
 }
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Book = (props) => {
+  console.log(props);
+  return (
+    <article className='book'>
+      <img alt="" width="200" src={props.img}></img>
+      <h1>{props.title}</h1> 
+      <h4>{props.author}</h4>
+    </article>
+  )
+}
+
+ReactDom.render(<BookList />, document.getElementById("root")); 
