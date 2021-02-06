a96ffdfd6464e84fb3975a4c62e10176731e2135
Jari Haapasaari <jthaapas@student.jyu.fi>
2021-02-06 23:51:34 +0200

    2a kurssitiedot step6

--- osa2/harjoittelu/practice-app/src/index.js
+++ osa2/harjoittelu/practice-app/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Note from './components/Note'
+import App from './components/App'
 
 const notes = [
   {
@@ -21,23 +23,6 @@ const notes = [
     important: true
   }
 ]
-
-const App = (props) => {
-  const { notes } = props
-
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-          {notes.map(note => 
-            <li key={note.id}>
-              {note.content}
-            </li>
-          )}
-      </ul>
-    </div>
-  )
-}
 
 ReactDOM.render(
   <App notes={notes} />,
