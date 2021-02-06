5e8b94dba04059117ced4e56420e17f4ef5e9890
Jari Haapasaari <jthaapas@student.jyu.fi>
2021-02-07 00:12:32 +0200

    2a kurssitiedot step 7

--- osa2/kurssitiedot/src/components/Total.js
+++ osa2/kurssitiedot/src/components/Total.js
@@ -1,20 +1,18 @@
 import React from 'react'
 
-function myAddition(total, num) {
-    return total + num;
-}
-
 const Total = (props) => {
 
-    var length = props.course.parts.length;
-    var totalExercises = 0;
-    var numbers = []
+    // console.log(parts)
 
-    for (var i = 0; i < length; i++) {
-        numbers.push(props.course.parts[i].exercises);
-    }
+    /*
+    const totalExercises = parts.reduce(function(sum, element) {
+      console.log("element:", sum, element )
+      return sum + element.exercises
+    }, 0)
+    */
 
-    totalExercises = numbers.reduce(myAddition);
+    var parts = props.course.parts;
+    const totalExercises = parts.reduce((sum, element) => sum + element.exercises, 0)
 
     return (
       <div>
